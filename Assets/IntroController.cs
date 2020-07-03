a048476f1dd4bdbb2f09e90c83e7b1afc2fdf704
nachocifu <ignacio.cifuentes948@gmail.com>
2020-07-03 14:15:32 -0300

    included intro to game

---
+++ Assets/IntroController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > 60)
+            gameObject.SetActive(false);
+    }
+    
+    public void OnPointerClick()
+    {
+        gameObject.SetActive(false);
+    }
+}
